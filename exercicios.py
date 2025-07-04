5506af7d7a58aea688ed7c31ee167e375e915cbf
Jaitan - Trabalho <23dataengineer@gmail.com>
2025-07-04 15:46:44 -0300

    exercicio 02 ok

--- exercicios.py
+++ exercicios.py
@@ -1,14 +1,20 @@
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
-#try
-numero_01 = int(input("Digite o primeiro número:"))
-numero_02 = int(input("Digite o segundo número:"))
-resultado = print("O resultado da soma é:", numero_01 + numero_02)
-#except
-#print("A soma foi efetuada com sucesso!")
+# try:
+#     numero_01 = int(input("Digite o primeiro número:"))
+#     numero_02 = int(input("Digite o segundo número:"))
+#     resultado = print("O resultado da soma é:", numero_01 + numero_02)
+# except:
+#     print("Erro: Por favor, digite apenas números inteiros")
 
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+numero = float(input("Insira um número:"))
+resultado = numero % 5
+print("O resultado é:" , resultado)
+
+
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
